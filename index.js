b1da5a62748614645ed7852f9611ba47f1550206
+ <+>
2022-11-19 13:53:25 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,22 @@
+function introduction(name){
+    let msg = "Hi, my name is "+ name + "."
+    return msg;}
+    console.log(introduction("Aki"));
+    function introductionWithLanguage(name, language) {
+        let msg = "Hi, my name is " + name + " and I am learning to program in " + language + "."
+        return msg;
+        console.log(introductionWithLanguage(Aki, Ember.js));
+      }
+      function introductionWithLanguageOptional(name, language = "JavaScript") {
+        let msg = "Hi, my name is " + name + " and I am learning to program in " + language + "."
+        return msg;
+        console.log(introductionWithLanguage(name, language));
+      }
+    
+    
+    
+    
+    
+    
+    
+    
